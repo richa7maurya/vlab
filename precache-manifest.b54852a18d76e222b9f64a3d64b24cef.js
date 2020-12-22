ef93171c530e6f0e562af2c0637a13fae51b7b4f
richa7maurya <55183915+richa7maurya@users.noreply.github.com>
2020-12-23 00:07:12 +0530

    Add files via upload

---
+++ precache-manifest.b54852a18d76e222b9f64a3d64b24cef.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8136d7c6cd55f9c3f9d201349eebce3c",
+    "url": "/vlab/index.html"
+  },
+  {
+    "revision": "740c7ace8c34d0bf4227",
+    "url": "/vlab/static/css/2.751616e6.chunk.css"
+  },
+  {
+    "revision": "1ca5a06778bbd745a318",
+    "url": "/vlab/static/css/main.81cc39d8.chunk.css"
+  },
+  {
+    "revision": "740c7ace8c34d0bf4227",
+    "url": "/vlab/static/js/2.439b4d4c.chunk.js"
+  },
+  {
+    "revision": "1ca5a06778bbd745a318",
+    "url": "/vlab/static/js/main.80209348.chunk.js"
+  },
+  {
+    "revision": "ac5193ba5622f1a19619",
+    "url": "/vlab/static/js/runtime~main.cf877b7d.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/vlab/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/vlab/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/vlab/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/vlab/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "346988f13fdbe86fd3ef9b183bf2d6bb",
+    "url": "/vlab/static/media/akash.346988f1.jpeg"
+  },
+  {
+    "revision": "c75cb46d309df0a1c0df80ffba38196c",
+    "url": "/vlab/static/media/ghanshyam.c75cb46d.jpeg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/vlab/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/vlab/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/vlab/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/vlab/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/vlab/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "84fddc1d78cd7df79f353d235910f940",
+    "url": "/vlab/static/media/hitesh.84fddc1d.jpeg"
+  },
+  {
+    "revision": "b11f832e469a653019f0f8de2ca3d17d",
+    "url": "/vlab/static/media/kunal.b11f832e.jpeg"
+  },
+  {
+    "revision": "6e64533e3d883fbda74ea311347cb0a2",
+    "url": "/vlab/static/media/mam.6e64533e.jpeg"
+  },
+  {
+    "revision": "4eac2000e212ba8aa8c3ba082d2edde6",
+    "url": "/vlab/static/media/mescoe_logo.4eac2000.png"
+  },
+  {
+    "revision": "237ce3b34a07413a4891117b1b212b23",
+    "url": "/vlab/static/media/parag.237ce3b3.jpeg"
+  },
+  {
+    "revision": "37c38062812eb1ee3a02fe951ea652b6",
+    "url": "/vlab/static/media/richa.37c38062.jpg"
+  },
+  {
+    "revision": "b20b218867c7dbd20a88e482dc29aac5",
+    "url": "/vlab/static/media/sidebar-3.b20b2188.jpg"
+  },
+  {
+    "revision": "b0150be89887dd6d93c3de4a33325cfb",
+    "url": "/vlab/static/media/sudesh.b0150be8.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vlab/precache-manifest.b54852a18d76e222b9f64a3d64b24cef.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vlab/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
